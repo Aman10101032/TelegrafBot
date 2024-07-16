a75eed5b9a2f982b2d8085948e50fcfcc4ba0480
Aman <amandosaliev111010@gmail.com>
2024-07-16 13:19:52 +0600

    release 2.0

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    "telegramToken": "{YOUR_BOT_TOKEN}",
+    "catUrl": "https://api.thecatapi.com/v1/images/search",
+    "weatherUrl": "https://api.weatherapi.com/v1/current.json?key={YOUR_BOT_TOKEN}&q=" //f615f74b6aa0421db8d185039241507
+}
